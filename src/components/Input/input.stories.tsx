24ab1fb2a57dab12bae3ed30b0b35e8fb4100126
winner <921876203@qq.com>
2023-04-21 08:32:18 +0800

    update：新增input组件，input.stories.tsx

---
+++ src/components/Input/input.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './input';
+const meta: Meta<typeof Input> = {
+    title: 'EVIL/Input',
+    component: Input,
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <div style={{ width: '224px' }}>
+                {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+                <Story />
+            </div>
+        ),
+    ]
+}
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const 基础用法: Story = {
+    // args: {
+    //     children: 'Primary',
+    // },
+
+};
+export const 禁用状态: Story = {
+    args: {
+        disabled: true
+    },
+};
+export const 可清空的输入框: Story = {
+    args: {
+        clearable: true,
+    },
+};
+export const 带icon的输入框: Story = {
+    args: {
+        suffixIcon: "icon-circle-close",
+        prefixIcon: "icon-date"
+    },
+};
+/**
+ * ##### 带有图标标记输入类型
+ */
+export const 传入icon组件的输入框: Story = {
+    args: {
+        suffixIcon: <i className='iconfont icon-circle-close'></i>,
+        prefixIcon: <i className='iconfont icon-date'></i>,
+    },
+};
--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,15 +1,92 @@
-import React, { useState } from "react";
+import React, { useState,FocusEvent, useEffect,MouseEvent } from "react";
 import classNames from "classnames";
-interface IInputProps {
+export interface IInputProps extends Omit<React.InputHTMLAttributes<HTMLElement>, 'size'> {
     className?: string;
+    /**输入框尾部图标，支持传入jsx类型或者string类型 */
+    suffixIcon?: string | React.ReactNode;
+    /**输入框头部图标，支持传入jsx类型或者string类型 */
+    prefixIcon?: string | React.ReactNode;
+    clearable?: boolean
 }
-type NativeInputProps = IInputProps & React.InputHTMLAttributes<HTMLElement>
-const Input: React.FC<Partial<NativeInputProps>> = (props) => {
+type IconType = "prefix" | "suffix"
+export const Input: React.FC<IInputProps> = (props) => {
+
     const [inputValue, setInputValue] = useState("")
-    const { className, ...restProps } = props
+    const [clearIconShow, setClearIconShow] = useState(false)
+    const { className, suffixIcon, prefixIcon, clearable, ...restProps } = props
     const classes = classNames('evil-input', className)
+    const innerInputClasses = classNames('evil-inner_input', {
+        'is-disabled': restProps.disabled,
+        'evil-inner_input--suffix': !!suffixIcon,
+        'evil-inner_input--prefix': !!prefixIcon
+    })
+    function renderIcon(IconType: IconType, Icon: string | React.ReactNode,functionType?:string) {
+        function clearableHandler(){
+            setInputValue("")
+        }
+        function clickHandler(event:MouseEvent){
+            event.stopPropagation()
+            if(functionType&&functionType=='clearable'){
+                clearableHandler()
+            }
+        }
+        if (!Icon) {
+            return ''
+        } else {
+            const IconBoxClass = classNames('evil-input-icon', `${IconType}-icon`)
+            if (typeof Icon === 'string') {
+                const iconClass = classNames('iconfont', Icon)
+                console.log('渲染')
+                return (
+                    <span className={IconBoxClass}>
+                        <i className={iconClass} onClick={clickHandler} onMouseDown={(event)=>event.preventDefault()}></i>
+                    </span>
+                )
+            } else {
+                return (
+                    <span className={IconBoxClass}>
+                        {Icon}
+                    </span>
+                )
+            }
+        }
+    }
+    function onMouseEnterHandler() {
+        if (inputValue) {
+            setClearIconShow(true)
+        } else {
+            setClearIconShow(false)
+        }
+    }
+    function onMouseLeaveHandler() {
+        setClearIconShow(false)
+    }
+    function onBlurHandler(event:FocusEvent<HTMLElement>){
+        setClearIconShow(false)
+        restProps.onBlur&&restProps.onBlur(event)
+    }
+    useEffect(()=>{
+        if(inputValue){
+            setClearIconShow(true)
+        }else{
+            setClearIconShow(false)
+        }
+    },[inputValue])
     return (
-        <input type="text" value={inputValue} onChange={(event) => { setInputValue(event.target.value) }} className={classes} {...restProps} />
+        <div className={classes} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler}>
+            {renderIcon('prefix', prefixIcon)}
+            <input 
+            placeholder="请输入" 
+            type="text" 
+            value={inputValue} 
+            onChange={(event) => { setInputValue(event.target.value) }} 
+            onBlur={onBlurHandler}
+            className={innerInputClasses} 
+            {...restProps} 
+            />
+            {renderIcon('suffix', suffixIcon)}
+            {clearable && clearIconShow && renderIcon('suffix', 'icon-circle-close','clearable')}
+        </div>
     )
 }
 
